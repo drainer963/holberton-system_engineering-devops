de8dcb96146b5c263145e0567bfa06b0760a51c0
trevor-ofarrell <744@holbertonschool.com>
2020-02-03 13:20:43 -0800

    adding json dump of all employees task progress

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""return information about all employees TODO list progress
+as a dictionary of lists of dictionarires"""
+import requests
+import sys
+import json
+
+
+newdict = {}
+name = 1
+for user in range(0, 9):
+    ul = []
+    req = requests.get(
+        "{}{}".format('https://jsonplaceholder.typicode.com/users/', name))
+    reqdata = req.json()
+    todo = requests.get('https://jsonplaceholder.typicode.com/todos')
+    tododata = todo.json()
+    username = reqdata['name']
+
+    for task in tododata:
+        if task['userId'] == int(name):
+            updatedtask = {}
+            updatedtask.update({"username": username})
+            updatedtask.update({"task": task['title']})
+            updatedtask.update({"completed": task['completed']})
+            ul.append(updatedtask)
+    mydict = {str(name): ul}
+    newdict.update(mydict)
+    name += 1
+
+with open('todo_all_employees.json', 'w') as fp:
+    dump = json.dump(newdict, fp)
