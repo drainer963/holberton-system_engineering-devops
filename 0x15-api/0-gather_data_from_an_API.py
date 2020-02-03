9ca98a2c4d16f427dc9dc4864ebee8f35cb74ab5
trevor-ofarrell <744@holbertonschool.com>
2020-02-03 10:11:11 -0800

    inital commit

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""given an employee ID, return information about his/her TODO list progress"""
+import requests
+import sys
+import json
+
+count = 0
+ccount = 0
+ul = []
+cl = []
+name = sys.argv[1]
+
+req = requests.get(
+    "{}{}".format('https://jsonplaceholder.typicode.com/users/', name))
+reqdata = req.json()
+todo = requests.get('https://jsonplaceholder.typicode.com/todos')
+tododata = todo.json()
+
+for task in tododata:
+    if task['userId'] == int(name):
+        count += 1
+        ul.append(task)
+
+for task in ul:
+    if task['completed'] is True:
+        ccount += 1
+        cl.append(task)
+
+print("{} {} {}({}/{}):".format(
+    "Employee", reqdata['name'], "is done with tasks", ccount, count))
+
+for tasks in cl:
+    print("{} {}".format('\t', tasks['title']))
