af0762bb11e86b813b93f20010dd6e96257b436f
trevor-ofarrell <744@holbertonschool.com>
2020-02-03 19:05:07 -0800

    refactor

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -12,8 +12,6 @@ if __name__ == "__main__":
     req = requests.get(
         "{}{}".format('https://jsonplaceholder.typicode.com/users/', user_id))
     reqdata = req.json()
-    USER_ID = reqdata.get('id')
-    USERNAME = reqdata.get('name')
     todo = requests.get('https://jsonplaceholder.typicode.com/todos')
     tododata = todo.json()
 
@@ -21,11 +19,14 @@ if __name__ == "__main__":
         if task.get('userId') == int(user_id):
             ul.append(task)
 
-    with open(str(USER_ID) + '.csv', 'w') as data_file:
+    with open(str(user_id) + '.csv', 'w') as data_file:
         csv_writer = csv.writer(data_file, quoting=csv.QUOTE_ALL)
 
-        for tasks in ul:
-            TASK_TITLE = tasks.get('title')
-            TASK_COMPLETED_STATUS = tasks.get('completed',  False)
+        for task in ul:
             csv_writer.writerow(
-                [USER_ID, USERNAME, TASK_COMPLETED_STATUS, TASK_TITLE])
+                [
+                    task.get('userId'),
+                    reqdata.get('name'),
+                    task.get('completed'),
+                    task.get('title')
+                ])
